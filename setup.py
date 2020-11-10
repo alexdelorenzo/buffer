83f8a683585b7f976ec2c7b77ab5acb2a3a28d78
alexdelorenzo <you@example.com>
2020-11-09 23:13:50 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme = Path('README.md').read_text()
 
 
 setup(name="buffer",
-      version="0.1.1",
+      version="0.2.0",
       description="A stream buffer backed by a spooled temporary file.",
       long_description=readme,
       long_description_content_type="text/markdown",
